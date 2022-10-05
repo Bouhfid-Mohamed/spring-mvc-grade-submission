dd48e54f8a9f51e50b9822d6bce8400185a54497
Mohamed Bouhfid <bouhfidenim@gmail.com>
2022-10-05 17:07:28 +0200

    stater project spring mvc

---
+++ src/main/java/com/bouhfid/springmvcgradesubmission/SpringMvcGradeSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.bouhfid.springmvcgradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcGradeSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcGradeSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bouhfid/springmvcgradesubmission/SpringMvcGradeSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bouhfid.springmvcgradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcGradeSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
